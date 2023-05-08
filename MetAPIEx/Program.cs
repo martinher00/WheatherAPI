c29f2256f5e2828bd51ebfb66426a1c4b81eef9a
Per A. Bergersen <pab2222@hotmail.com>
2023-05-08 08:55:44 +0200

    init

--- MetAPIEx/Program.cs
+++ MetAPIEx/Program.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MetAPIEx
@@ -17,14 +18,16 @@ namespace MetAPIEx
             p.GetStuff();
         }
 
-        public int GetStuff()
+        
+
+        public int GetStuff()//denne har int, så vil da kunne returnere et heltall. 
         {
         //http://jsonviewer.stack.hu/
         //https://peterdaugaardrasmussen.com/2022/01/18/how-to-get-a-property-from-json-using-selecttoken/
             //create the httpwebrequest
             var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://api.met.no/weatherapi/nowcast/2.0/complete?lat=59.9333&lon=10.7166"); 
 
-            //the usual stuff. run the request, parse your json
+            //the usual stuff. run the request, parse your json. with error handling
             try
             {
                 httpWebRequest.ContentType = "application/json";
@@ -36,19 +39,14 @@ namespace MetAPIEx
                 {
                     var result = streamReader.ReadToEnd();
                     JObject jObj = JObject.Parse(result);
-                    //JToken data = jObj.SelectToken("path");
-                    //int valuepm1 = data.Value<int>("keyname");//key name - getting key.value
-                    //int valuepm25 = data.Value<int>("pm25");
-                    //int radonValue = data.Value<int>("radonShortTermAvg");
-                    // inn i db
-                  
+                    JToken data = jObj.SelectToken("properties.timeseries[0].data.instant.details");
+
+                    //double temp = data.Value<double>("air_temperature");//key name står i " " - getting key.value
                 }
-                return 0;
+                return 0;//returner ønsket verdi
             }
             catch { Exception ex; }
-            return 0;
+            return 0;//returner ønsket verdi
         }
-        
-
     }
 }
