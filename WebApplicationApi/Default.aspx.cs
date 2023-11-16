70943e0ecd826bfc84bec0678cd97aabbd758774
nitram <nitram@MARTINPC>
2023-11-16 13:42:33 +0100

    la til reference til buslayer i aspx fil

--- WebApplicationApi/Default.aspx.cs
+++ WebApplicationApi/Default.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using BusLayer;
 
 namespace WebApplicationApi
 {
@@ -11,7 +12,7 @@ namespace WebApplicationApi
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            bu
+            
         }
     }
 }
